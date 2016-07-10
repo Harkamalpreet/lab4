d82791f9a8b87531419ea3519881a53b6e7288d8
Harkamalpreet <kaurharkamal96@gmail.com>
2016-07-10 18:09:53 -0400

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,43 @@
+/**
+ * @ author : harkamalpreet kaur
+ * @ student number : 300871422
+ * @ fileName : app.js
+ * @ Lab4
+ * @ comp125
+ * @ summer 2016, centennial college
+ */
+
+/**
+ * This is an self executing function
+ * A.K.A IFEE
+ */
+(function () {
+    /**
+     * Hooking needed elements on the webpage using javascript
+     */
+    var h1 = document.getElementById("h1");
+    h1.innerHTML = "Console Checking Form";
+    var legend = document.getElementById("legend");
+    legend.innerHTML = "300871422";
+    var form = document.getElementById("form");
+    var username;
+    var password;
+    var submitButton = document.getElementById("submitButton");
+    submitButton.innerHTML = "Login";
+    
+    form.addEventListener('submit', function (e) {
+
+        //prevent the normal submission of the form
+        e.preventDefault();
+        console.log("App Started");
+        /**
+         * @ creating object UserObject
+         */
+        var UserObject = {
+            UserName : document.getElementById("username").value,
+            Password : document.getElementById("password").value
+        }
+        console.log(UserObject.UserName);
+        console.log(UserObject.Password);
+    });
+})();
